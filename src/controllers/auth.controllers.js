aed8a11d634dd5c47b52d21d126f34224180dec8
Yisus_dev <jesusdavid0518@hotmail.com>
2024-05-15 17:17:43 -0500

    add login and logout controllers + routes

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -6,35 +6,82 @@ import { createAccessToken } from "../libs/jwt.js";
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-   
-   const passwordHash = await bcrypt.hash(password, 10);
-   
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
-        username,
-        email,
-        password: passwordHash,
+      username,
+      email,
+      password: passwordHash,
     });
-// saving the user
+    // saving the user
     const userSaved = await newUser.save();
-     //create token
-     const token = await createAccessToken({ 
+    //create token
+    const token = await createAccessToken({
       id: userSaved._id,
     });
 
     res.cookie("token", token, {
-        //    httpOnly: process.env.NODE.ENV !== "development",
-        //    secure: true,
-        //    sameSite: "none",
+      //    httpOnly: process.env.NODE.ENV !== "development",
+      //    secure: true,
+      //    sameSite: "none",
     });
 
     res.json({
-        id: userSaved._id,
-        username: userSaved.username,
-        email: userSaved.email,
-          })
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
   } catch (error) {
     console.log("Error en el registro de usuario: ", error);
   }
 };
 
-export const login = (req, res) => res.send("mi primer consulta jaja");
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({message:"user not found"});
+
+
+    const isMatch= await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch) {
+return res.status(400).json({
+  message: ["password incorrect"],
+});
+    }
+   
+   
+    //create token
+    const token = await createAccessToken({
+      id: userFound._id,
+    });
+
+    res.cookie("token", token, {
+      //    httpOnly: process.env.NODE.ENV !== "development",
+      //    secure: true,
+      //    sameSite: "none",
+    });
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+res.status(500).json({ message: error.message});
+  }
+};
+
+
+export const logout = async (req, res) => {
+  res.cookie("token", "",{
+    httpOnly: true,
+    secure: true,
+    expires: new Date(0),
+      });
+      return res.sendStatus(200);
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {login, register} from "../controllers/auth.controllers.js"
+import {login, register, logout} from "../controllers/auth.controllers.js"
 
 
 const router = Router();
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
 
 
 export default router;
