05b8acf857a47f8b9782ca2034c600c21e741570
Yisus_dev <jesusdavid0518@hotmail.com>
2024-05-29 18:57:08 -0500

    t pull origin main Merge branch 'main' of github.com:YisusdevWeb/CRUD_Node.js into add_token_create Merge branch 'main' of github.com:YisusdevWeb/CRUD_Node.js into add_token_create
    Resolved conflicts and merged updates from the main branch

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -37,24 +37,21 @@ export const register = async (req, res) => {
   }
 };
 
-
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const userFound = await User.findOne({ email });
 
-    if (!userFound) return res.status(400).json({message:"user not found"});
+    if (!userFound) return res.status(400).json({ message: "user not found" });
 
-
-    const isMatch= await bcrypt.compare(password, userFound.password);
+    const isMatch = await bcrypt.compare(password, userFound.password);
 
     if (!isMatch) {
-return res.status(400).json({
-  message: ["password incorrect"],
-});
+      return res.status(400).json({
+        message: ["password incorrect"],
+      });
     }
-   
-   
+
     //create token
     const token = await createAccessToken({
       id: userFound._id,
@@ -72,16 +69,32 @@ return res.status(400).json({
       email: userFound.email,
     });
   } catch (error) {
-res.status(500).json({ message: error.message});
+    res.status(500).json({ message: error.message });
   }
 };
 
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+  if (!token) return res.send(false);
+
+  jwt.verify(token, TOKEN_SECRET, async (error, user) => {
+    if (error) return res.sendStatus(401);
+
+    const userFound = await User.findById(user.id);
+    if (!userFound) return res.sendStatus(401);
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  });
+};
 
 export const logout = async (req, res) => {
-  res.cookie("token", "",{
+  res.cookie("token", "", {
     httpOnly: true,
     secure: true,
     expires: new Date(0),
-      });
-      return res.sendStatus(200);
-}
+  });
+  return res.sendStatus(200);
+};
